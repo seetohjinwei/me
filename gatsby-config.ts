f588269fbaedcc4338496bcfc339036673ece5cd
Jin Wei <seetohjinwei@gmail.com>
2022-10-21 17:03:37 +0800

    Add favicon

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -27,6 +27,18 @@ const config: GatsbyConfig = {
         ],
       },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `GatsbyJS`,
+        short_name: `GatsbyJS`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+        icon: `src/images/favicon.png`,
+      },
+    },
   ],
 };
 
