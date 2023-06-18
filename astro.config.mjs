8ed91e21bd3e719410a63e22f189077ca02000e7
Jin Wei <seetohjinwei@gmail.com>
2023-06-18 16:42:57 +0800

    Add robots.txt / sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,21 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   markdown: {
-    remarkPlugins: [
-      'remark-math',
-      'remark-gfm',
-      'remark-sectionize'
-    ],
+    remarkPlugins: ["remark-math", "remark-gfm", "remark-sectionize"],
     rehypePlugins: [
-      ['rehype-katex', {
-      // Katex plugin options
-      }]
-    ]
-  }
+      [
+        "rehype-katex",
+        {
+          // Katex plugin options
+        },
+      ],
+    ],
+  },
+  site: "https://jinwei.dev",
+  integrations: [sitemap()],
 });
